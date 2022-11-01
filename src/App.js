ff90cfb7da9fa48a098d8adb459d307a9abe94b5
DanielTorres <torres.daniel.adr2@gmail.com>
2022-11-01 10:44:28 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AccountList from "./components/accountList/AccountList";
+import AccountDetail from "./components/accountDetail/AccountDetail";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="box">
+      <BrowserRouter>
         <div className="banner"></div>
-        <AccountList></AccountList>
-      </div>
+        <div className="box">
+          <Routes>
+            <Route path={"/"} element={<AccountList></AccountList>}></Route>
+            <Route
+              path={"/detail/:id"}
+              element={<AccountDetail></AccountDetail>}
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/accountDetail/AccountDetail.js
+++ src/components/accountDetail/AccountDetail.js
@@ -1,19 +1,57 @@
-export default function AccountDetail({ accountData }) {
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function AccountDetail() {
+  const { id } = useParams();
+  const url = "https://api.npoint.io/97d89162575a9d816661";
+  const [account, setAccount] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setAccount(
+          data.cuentas
+            .filter(
+              (e) =>
+                validAccountCurrency(e) &&
+                validAccountNumber(e) &&
+                validAccountType(e)
+            )
+            .find((element) => element.n === id)
+        );
+      });
+  }, []);
+
+  function validAccountType(e) {
+    return e.tipo_letras === "CC" || e.tipo_letras === "CA";
+  }
+
+  function validAccountNumber(e) {
+    return e.n.trim();
+  }
+  function validAccountCurrency(e) {
+    return e.moneda === "$" || e.moneda === "u$s";
+  }
+
   return (
     <div>
       <h4>Consulta de saldo</h4>
       <h1>Este es tu saldo actual</h1>
       <p>
-        Saldo de la cuenta: {accountData.moneda} {accountData.saldo}
+        Saldo de la cuenta: {account.moneda} {account.saldo}
       </p>
       <p>
         Tipo de Cuenta:{" "}
-        {accountData.tipo_letras === "CC"
-          ? "Cuenta corriente"
-          : "Caja de Ahorro"}{" "}
-        en {accountData.moneda === "$" ? "Pesos" : "Dolares"}
+        {account.tipo_letras === "CC" ? "Cuenta corriente" : "Caja de Ahorro"}{" "}
+        en {account.moneda === "$" ? "Pesos" : "Dolares"}
       </p>
-      <p>Numero de cuenta: {accountData.n}</p>
+      <p>Numero de cuenta: {account.n}</p>
+      <Link to="/">
+        <button className="accountBox">
+          <p>salir</p>
+        </button>
+      </Link>
     </div>
   );
 }
